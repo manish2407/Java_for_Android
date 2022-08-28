5a6d5ac0d110417d5fa68ddb5cc08169579d798a
manish2407 <manishms2407@gmail.com>
2022-08-28 20:12:36 +0530

    Initial commit

---
+++ src/Basic_Java/Basic.java
@@ -0,0 +1,27 @@
+package Basic_Java;
+
+import java.io.IOException;
+    public class Basic {public static void main(String[] args) throws IOException {
+        byte b = 3;
+        short s = 100;
+        int i = 12345;
+        long l = 12345678;
+        float f = 12.2f;
+        double d = 123.45678;
+        boolean bl= true;
+        char c = 'a';
+        String str = "Manish";
+        //constant
+        final int a = 123;
+        //print variables
+        System.out.println(" value of b = "+ b);
+        System.out.println(" value of s = "+ s);
+        System.out.println(" value of i = "+ i);
+        System.out.println(" value of l = "+ l);
+        System.out.println(" value of f = "+ f);
+        System.out.println(" value of d = "+ d);
+        System.out.println(" value of bl = "+ bl);
+        System.out.println(" value of c = "+ c);
+        System.out.println(" value of str = "+ str);
+    }
+}
